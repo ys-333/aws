258fb36268e0c9ecf8bfb9b3aead876ff9738391
ys-333 <yashwantsing333@gmail.com>
2024-02-09 02:56:10 +0530

    pm2 integration typescript

--- build/index.js
+++ build/index.js
@@ -1,14 +1,24 @@
 'use strict';
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = __importDefault(require("express"));
-var logger_1 = __importDefault(require("./utils/logger"));
-var User = {
-    name: 'yashwant',
-    age: 12,
-};
-logger_1.default.info('user');
-console.log(User);
-var app = (0, express_1.default)();
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+let name = 'yashwant';
+app.get('/home', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    return res.status(200).json();
+}));
+app.listen(5000, () => {
+    console.log('server started at port 3000');
+});
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
 'use strict'
 import express, { Application, Request, Response } from 'express'
 
-const app = express()
+const app: Application = express()
+
+app.use(express.json())
 
 let name: string = 'yashwant'
 
 app.get(
   '/home',
   async (req: Request, res: Response): Promise<Response> => {
-    return res.status(200).json({ message: 'we are going all well' })
+    return res.status(200).json()
   },
 )
 
-app.listen(3000, (): void => {
+app.listen(5000, (): void => {
   console.log('server started at port 3000')
 })
+
+/*
+   "start": "npm run build && node build/index.js",
+    "start:dev": "npx nodemon",
+    "build": "rimraf ./build && tsc",
+    "test": "echo \"Error: no test specified\" && exit 1"
+*/
